45f1468bfc85756a8936b3f1b59dd8feafd5ae56
terdail <terdail@proton.me>
2023-03-27 18:04:37 +0300

    CORS fix

--- backend/cmd/daosrv/main.go
+++ backend/cmd/daosrv/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"dao-backend/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func init() {
@@ -17,6 +20,17 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     strings.Split(os.Getenv("SERVER_CORS_ALLOW_ORIGINS"), ","),
+		AllowMethods:     strings.Split(os.Getenv("SERVER_CORS_ALLOW_METHODS"), ","),
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	r.GET("/", controllers.Index)
 	r.GET("/voting/:address", controllers.GetVoting)
 	r.POST("/voting", controllers.CreateVoting)
